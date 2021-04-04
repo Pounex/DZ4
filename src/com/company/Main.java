623271007299e19d7f9f018f09a2cd0a76fc88c9
asus <nik.kardymon@mail.ru>
2021-04-04 17:02:04 +0300

    5 дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,21 @@ public class Main {
         Rectangle rec3 = new Rectangle(4, 6);
         Rectangle recX = new Rectangle(4, 5);
 
-        rec1.printArea();
-        rec1.printRectangleKind();
-        System.out.println(rec1.isTheSameRectangle(recX));
+//        rec1.printArea();
+//        rec1.printRectangleKind();
+//        System.out.println(rec1.isTheSameRectangle(recX));
+//
+//        rec2.printArea();
+//        rec2.printRectangleKind();
+//        System.out.println(rec2.isTheSameRectangle(recX));
+//
+//        rec3.printArea();
+//        rec3.printRectangleKind();
+//        System.out.println(rec3.isTheSameRectangle(recX));
 
-        rec2.printArea();
-        rec2.printRectangleKind();
-        System.out.println(rec2.isTheSameRectangle(recX));
+        Rectangle.printRectanglesCount();
+        Rectangle.printClassName(true);
+        Rectangle.printClassName(false);
 
-        rec3.printArea();
-        rec3.printRectangleKind();
-        System.out.println(rec3.isTheSameRectangle(recX));
     }
 }
--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -4,16 +4,20 @@ public class Rectangle {
 
     public double x;
     public double y;
-
+    private static int createdRectangles = 0;
+    private static final String RUSSIAN_CLASS_NAME = "Прямоугольник";
+    private static final String ENGLISH_CLASS_NAME = "Rectangle";
 
     Rectangle(double x, double y) {
         this.x = x;
         this.y = y;
+        createdRectangles++;
     }
 
     Rectangle(double x) {
         this.x = x;
         this.y = x;
+        createdRectangles++;
     }
 
     double calculateArea() {
@@ -34,6 +38,28 @@ public class Rectangle {
 
     boolean isTheSameRectangle(Rectangle recX) {
         return this.x == recX.x && this.y == recX.y;
+
+    }
+    //////////////////////////////////////////////////////
+
+    static void printRectanglesCount() {
+        if (createdRectangles / 10 == 1) {
+            System.out.println("Всего было создано " + createdRectangles + " прямоугольников");
+        } else if (createdRectangles % 10 == 1) {
+            System.out.println("Всего было создано " + createdRectangles + " прямоугольник");
+        } else if (createdRectangles % 10 > 1 && createdRectangles % 10 < 5) {
+            System.out.println("Всего было создано " + createdRectangles + " прямоугольника");
+        } else if ((createdRectangles % 10 > 4 && createdRectangles % 10 < 10) || (createdRectangles == 0)) {
+            System.out.println("Всего было создано " + createdRectangles + " прямоугольников");
+        }
+
+    }
+
+    static void printClassName(boolean printRussian) {
+        if (printRussian)
+            System.out.println(RUSSIAN_CLASS_NAME);
+
+        else System.out.println(ENGLISH_CLASS_NAME);
 
     }
 
